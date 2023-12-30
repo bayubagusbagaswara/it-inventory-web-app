4b8a917be48d0af2ed9001dce46c6e1627c449e8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-12-30 21:12:26 +0700

    Initial commit

---
+++ src/main/java/com/itinventory/ItInventoryWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.itinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItInventoryWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItInventoryWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itinventory/ItInventoryWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItInventoryWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
